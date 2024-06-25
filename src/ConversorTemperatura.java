7dc2aa3e355347617793dda00f07812388a17ce2
Sonia Suárez <ssuarezcode@outlook.com>
2024-06-25 14:40:24 -0500

    Adding refinement to ConversorTemperatura code.

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -1,13 +1,17 @@
 public class ConversorTemperatura {
     public static void main(String[] args) {
+        //Variable de temperatura en Celsius:
         double temperaturaCelsius = 22.0;
+
+        //Fórmula de conversión de Celsius a Farenheit:
         double temperaturaFarenheit = (temperaturaCelsius * 1.8) + 32;
 
+        //Mensaje con el resultado de la temperatura en Celsius y su conversión a Farenheit:
         String mensaje = String.format("La temperatura de %.1f Celsius es equivalente a %.1f Farenheit", temperaturaCelsius, temperaturaFarenheit);
-
         System.out.println(mensaje);
 
+        //Casteo de double a int para mostrar el valor de la temperatura en números enteros:
         int farenheitEntero = (int) temperaturaFarenheit;
-        System.out.println(farenheitEntero);
+        System.out.println("La temperatura en Farenheit entera es: " + farenheitEntero);
     }
 }
