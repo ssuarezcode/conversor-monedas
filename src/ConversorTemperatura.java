7e21ffbfda1c4d46abf47da2d520cee1bc81e401
Sonia Suárez <ssuarezcode@outlook.com>
2024-06-25 14:18:53 -0500

    Adding temperature conversor.

---
+++ src/ConversorTemperatura.java
@@ -0,0 +1,8 @@
+public class ConversorTemperatura {
+    public static void main(String[] args) {
+        int celsius = 21;
+        int temperatura = (int) ((celsius * 1.8) + 32);
+
+        System.out.println(temperatura);
+    }
+}
