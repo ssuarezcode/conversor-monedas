337002c7d449b1e52d3e85b54f0e1b6e5dc66539
Sonia Suárez <ssuarezcode@outlook.com>
2024-06-25 14:35:15 -0500

    Updating ConversorTemperatura logic.

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -1,9 +1,13 @@
 public class ConversorTemperatura {
     public static void main(String[] args) {
-        int celsius = 22;
-        int temperatura = (int) ((celsius * 1.8) + 32);
-        double farenheit = (double) temperatura;
+        double temperaturaCelsius = 22.0;
+        double temperaturaFarenheit = (temperaturaCelsius * 1.8) + 32;
 
-        System.out.println(farenheit);
+        String mensaje = String.format("La temperatura de %.1f Celsius es equivalente a %.1f Farenheit", temperaturaCelsius, temperaturaFarenheit);
+
+        System.out.println(mensaje);
+
+        int farenheitEntero = (int) temperaturaFarenheit;
+        System.out.println(farenheitEntero);
     }
 }
