bc8f03980362092590b158a0c6cc82227b5ed2d1
Sonia Suárez <ssuarezcode@outlook.com>
2024-06-25 14:21:46 -0500

    Updating ConversorTemperatura logic.

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -1,8 +1,9 @@
 public class ConversorTemperatura {
     public static void main(String[] args) {
-        int celsius = 21;
+        int celsius = 22;
         int temperatura = (int) ((celsius * 1.8) + 32);
+        double farenheit = (double) temperatura;
 
-        System.out.println(temperatura);
+        System.out.println(farenheit);
     }
 }
